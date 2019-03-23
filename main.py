4dc8325b0573196327c9a75db3507dad18b06fe1
intvalentin <47526377+intvalentin@users.noreply.github.com>
2019-03-23 16:52:52 +0200

    final version

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ import time
 def nextGraph():
         try:
             nextNode=[[0,0]]
-            caca =0
-            previousNode = 'caca'
+            edgeColorsAnimation = []
+            previousNode = 'something'
+
             i=0
             try:
                 currentNode = listTables.get(listTables.curselection())
@@ -28,18 +29,16 @@ def nextGraph():
                 print(e)
                 messagebox.showwarning(
                     'Error', "Please RESET the graph!")
-            while currentNode != '2.  Bucharest' and caca<10:
-                caca+=1
-        #print(datasetCost[int(nodeNumber[0])-1])
+            while True:
                 if DG.order() and i == 0:
                     description.config(text='Current Node-> ' +str(currentNode)+'\n' +'Next Node to choose-> ')
                     a.cla()
                     DG.clear()
-                    DG.add_node(listTables.get(listTables.curselection()))
-                    nx.draw_networkx(DG, pos, ax=a)
+                    DG.add_node(listTables.get(listTables.curselection()),rank=0)
+                    nx.draw_networkx(DG, pos, ax=a )
                     a.axis('off')
                     canvas.draw()
-                    #time.sleep(1.5)
+                    time.sleep(1)
                     i+=1
                 else:
                     nodeNumber = currentNode.split(". ")
@@ -49,37 +48,35 @@ def nextGraph():
                     for x in datasetCost[int(nodeNumber[0])-1]:
 
                         if x != -1:
-                            DG.add_weighted_edges_from([(currentNode, datasetNames[y][0], x)],length=x)
 
-                            nx.draw_networkx_labels(DG,pos,ax=a)
-                            nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),font_color='red',edge_color='r',ax=a)
-                            nx.draw_networkx(DG, pos, ax=a)
-                            canvas.draw()
-                            print('valiz'+str(nextNode[0][0]))
-                            if nextNode[0][0] == 0 and datasetNames[y][0] != previousNode:
+                            DG.add_weighted_edges_from([(currentNode, datasetNames[y][0], x)],length=x)
+                            nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),font_color='red',ax=a)
+                            if datasetNames[y][0] == previousNode:
+                                edgeColorsAnimation.append('g')
+                            else:
+                                edgeColorsAnimation.append('r')
+
+                            if nextNode[0][0] == 0 and datasetNames[y][0] != previousNode: #check if next node to choose allready exists
                                 nextNode = [[datasetNames[y][0],x+int(datasetNames[y][1])]]
-                                print(nextNode)
                                 description.config(text=description.cget('text')+str(nextNode[0][0]+' Cost: '+str(nextNode[0][1])))
-                            elif x+int(datasetNames[y][1])< nextNode[0][1] and datasetNames[y][0] != previousNode:
+                            elif x+int(datasetNames[y][1])< nextNode[0][1] and datasetNames[y][0] != previousNode: # choose the smallest cost node to bucharest
                                 nextNode = [[datasetNames[y][0],x+int(datasetNames[y][1])]]
-                                print(nextNode)
                                 description.config(text=description.cget('text')+' > '+str(nextNode[0][0]+' Cost: '+str(nextNode[0][1])))
-                            #time.sleep(1.5)
+
+                            nx.draw_networkx(DG, pos, ax=a,edge_color=edgeColorsAnimation)
+                            canvas.draw()
+                            time.sleep(1)
                         y+=1
 
                     previousNode = currentNode
                     currentNode = nextNode[0][0]
-                    print('PN: '+str(previousNode)+' CN: '+str(currentNode))
-                    print('currentNode : '+currentNode)
                     description.config(text=description.cget('text')+'\n'+'Current Node-> ' +str(currentNode)+'\n' )
                     if currentNode != '2.  Bucharest':
                         description.config(text=description.cget('text')+'Next Node to choose-> ')
+
                     nextNode=[[0,0]]
-
-
-
-
-
+                    if currentNode == '2.  Bucharest':
+                        break
 
         except Exception as e:
             print(e)
@@ -103,8 +100,11 @@ def resetGraph():
                 if y != -1:
                     DG.add_weighted_edges_from([(name, datasetNames[i][0], y)])
                 i+=1
-        nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),font_color='red',edge_color='r',ax=a)
-        nx.draw_networkx(DG, pos, ax=a)
+        nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),edge_color='r',ax=a)
+        nx.draw_networkx(DG, pos, ax=a,with_labels=False,edge_color='g')
+        # for p in pos:  # raise text positions
+        #     pos[p][1] += 0.07
+        nx.draw_networkx_labels(DG,pos,ax=a)
         canvas.draw()
 
 
@@ -129,10 +129,10 @@ if __name__ == '__main__':
     #DataStart
     DG = nx.DiGraph()
     datasetNames =[
-        ['1.  Arad',366]     ,['2.  Bucharest', 0]   ,['3.  Craiova', 160]  ,['4.  Dobreta', 242] ,['5.  Eforie',161],
-        ['6.  Fagaras', 176] ,['7.  Giurgiu', 77]    ,['8.  Hirsova', 151]  ,['9.  Iasi', 226]    ,['10. Lugoj', 244],
-        ['11. Mehadia', 241] ,['12. Neamt', 234]     ,['13. Oradea', 380]   ,['14. Pitesti', 10]  ,['15. Ramnicu Vilcea', 193],
-        ['16. Sibiu', 253]   ,['17. Timisoara', 392] ,['18. Urziceni', 80]  ,['19. Vaslui', 199]  ,['20. Zerind', 374]
+        ['1.  Arad [366]',366]     ,['2.  Bucharest', 0]   ,      ['3.  Craiova [160]', 160]  ,['4.  Dobreta [242]', 242] ,['5.  Eforie [161]',161],
+        ['6.  Fagaras [176]', 176] ,['7.  Giurgiu [77]', 77]    , ['8.  Hirsova [151]', 151]  ,['9.  Iasi [226]', 226]    ,['10. Lugoj [244]', 244],
+        ['11. Mehadia [241]', 241] ,['12. Neamt [234]', 234]     ,['13. Oradea [380]', 380]   ,['14. Pitesti [10]', 10]   ,['15. Ramnicu Vilcea [193]', 193],
+        ['16. Sibiu [253]', 253]   ,['17. Timisoara [392]', 392] ,['18. Urziceni [80]', 80]   ,['19. Vaslui [199]', 199]  ,['20. Zerind [374]', 374]
     ]
     nr =[1  ,2  ,3  ,4  ,5  ,6  ,7  ,8  ,9  ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 ,20 ]
 
@@ -168,13 +168,18 @@ if __name__ == '__main__':
             if y != -1:
                 DG.add_weighted_edges_from([(name, datasetNames[i][0], y)])
             i+=1
-    # print(DG.nodes())
-    # print('number of nodes in the graph:', DG.number_of_nodes())
 
-    pos = nx.spring_layout(DG,weight=300)
-    nx.draw_networkx(DG, pos,font_size=16, with_labels=False,ax=a)
+
+    pos = nx.spring_layout(DG,fixed = None)
+    nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),ax=a)
+
+    nx.draw_networkx(DG, pos,font_size=16, with_labels=False,edge_color='g',ax=a)
+    # for p in pos:  # raise text positions
+    #     pos[p][1] += 0.07
     nx.draw_networkx_labels(DG,pos,ax=a)
-    nx.draw_networkx_edge_labels(DG,pos,edge_labels=nx.get_edge_attributes(DG,'weight'),font_color='red',edge_color='r',ax=a)
+
+
+
     # EndData
     canvas = FigureCanvasTkAgg(f, master=root)
     canvas.draw()
